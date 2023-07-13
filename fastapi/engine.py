46b851cf69c358bcf73de16eb8cec75878b5f240
Tanush Yadav <53972008+Bliqlegend@users.noreply.github.com>
2023-07-13 20:12:24 +0530

    Update engine.py

--- fastapi/engine.py
+++ fastapi/engine.py
@@ -29,7 +29,7 @@ summary = """
 
   Fluent in Python, Dart, and Rust, Tanush demonstrates an adept understanding of web frameworks such as Django, FastAPI, VueJS, ReactJS, and the MERN stack. Their proficiency extends to mobile app development, primarily leveraging the Flutter platform.
 
-  In the domain of AI and Machine Learning, Tanush exhibits knowledge in Natural Language Processing (NLP) and Large Language Models, including OpenAI’s GPT-3.5/GPT-4. Their expertise extends to implementing Langchain wrappers for efficient utilization of these models, a prowess evidenced in projects like mavex.ai where WhatsApp chatbot interactions were substantially enhanced.
+  In the domain of AI and Machine Learning, Tanush exhibits knowledge in Natural Language Processing (NLP) and Large Language Models, including OpenAI’s GPT-3.5/GPT-4 and primarily managing LLMs with langchain. Their expertise extends to implementing Langchain wrappers for efficient utilization of these models, a prowess evidenced in projects like mavex.ai where WhatsApp chatbot interactions were substantially enhanced.
 
   Tanush's competency in Cloud and DevOps is notable, featuring a mastery of AWS cloud services, pipeline workflows/CI/CD, Docker, Ansible, and Terraform. Their expertise is manifested in managing server deployments and optimizing server scaling during peak traffic scenarios.
 
@@ -61,18 +61,48 @@ async def generate_application_message(item: Item):
     While writing, make sure to personalize your message, demonstrating genuine familiarity with the company and the role. Be concise and confident, using strong verbs and avoiding vague phrases. Do not end your message with a typical "looking forward" statement, instead, close with a note of thanks and encourage to check the portfolio and resume for reference. Keep the message and paragraphs short (Each Paragraph max : 150-200 characters) and clear. Don’t add anything which is outside the Company Information & JD given to you and be very deeply technical, which identifies that I genuinely a thing or two about the stack.
 
     Given these guidelines, generate a job application message that effectively introduces you, establishes a connection with the company, clearly states your intention, and ends with an offer of value.
-    an example DM for you to have an example to learn from : Dear Daniel,
+    some examples DM for you to have an example to learn from : 
+    
+    1. Dear Daniel,
+        
+        I'm Tanush Yadav, a seasoned Full Stack Developer with substantial experience across multiple stacks, including the MERN stack, Python, and Dart. My career trajectory has seen me thrive in roles ranging from internships to leading tech teams in high-growth startups such as mavex.ai.
+        
+        Byte's vision to revolutionize food delivery in Pakistan resonates strongly with my personal ambition to create impactful technology that betters everyday lives. The company's commitment to affordability and accessibility, as well as its impressive growth trajectory, are aspects that align with my aspirations. I'm particularly interested in the opportunity to contribute to the development of Byte's core internal-facing applications.
+        
+        The role requirements at Byte align perfectly with my skill set. My expertise in Full Stack Development, specifically in MEAN/MERN Stack, coupled with my hands-on experience with AWS and Git, equip me to deliver on the role's expectations. I also bring to the table a rich background in PostgreSQL and MySQL, thus ensuring a well-rounded approach to the development and optimization of Byte's internal tools and systems.
+        
+        Thank you for considering my application. I encourage you to review my portfolio and resume for a more detailed insight into my technical expertise and professional journey.
+        
+        Best Regards,
+        Tanush Yadav
 
-I'm Tanush Yadav, a seasoned Full Stack Developer with substantial experience across multiple stacks, including the MERN stack, Python, and Dart. My career trajectory has seen me thrive in roles ranging from internships to leading tech teams in high-growth startups such as mavex.ai.
+    2. Dear Niranjan,
 
-Byte's vision to revolutionize food delivery in Pakistan resonates strongly with my personal ambition to create impactful technology that betters everyday lives. The company's commitment to affordability and accessibility, as well as its impressive growth trajectory, are aspects that align with my aspirations. I'm particularly interested in the opportunity to contribute to the development of Byte's core internal-facing applications.
+        As a Full Stack Developer, I, Tanush Yadav, bring expertise in Python, Dart, Rust, and a keen understanding of diverse web frameworks. My experience spans from managing cloud operations to leading tech teams, an acumen sharpened over a multitude of roles.
+        
+        I am intrigued by Cedana's mission to revolutionize compute resource allocation through a systems-level approach. The prospect of contributing to a paradigm shift in areas like high-performance computing and numerical simulation fuels my interest in this role.
+        
+        Given my proficiency in Linux, C, and Go, along with my extensive experience in cloud services and high-availability systems, I believe I align well with Cedana's requirements. My exposure to AI and Machine Learning, and my practice with Torch/PyTorch could further be instrumental in dealing with large-scale ML model deployment challenges.
+        
+        Thank you for considering my application. I welcome you to review my portfolio and resume for more details about my professional journey.
+        
+        Best Regards,
+        Tanush Yadav
 
-The role requirements at Byte align perfectly with my skill set. My expertise in Full Stack Development, specifically in MEAN/MERN Stack, coupled with my hands-on experience with AWS and Git, equip me to deliver on the role's expectations. I also bring to the table a rich background in PostgreSQL and MySQL, thus ensuring a well-rounded approach to the development and optimization of Byte's internal tools and systems.
+    3. Dear Peter,
 
-Thank you for considering my application. I encourage you to review my portfolio and resume for a more detailed insight into my technical expertise and professional journey.
+        I am Tanush Yadav, a full-stack developer with rich experience in a broad range of technologies. My journey traverses a myriad of projects, from leading tech teams to enhancing business models, all of which have honed my technical acumen. I was particularly drawn to Wanderlog's mission, its value in travel, and its commitment to crafting tools to simplify the process.
+        
+        Wanderlog's focus on building an engineering and product-heavy team resonates with my skills. I am keen to contribute to your success by leveraging my expertise in JavaScript, Node.js, React, and React Native. Your culture of balancing work with life, and the emphasis on travel as a source of rejuvenation, speaks to my personal values.
+        
+        My skills align seamlessly with your requirements. My proficiency in building data pipelines, web and mobile applications, and managing databases can contribute significantly to the growth of Wanderlog. I am eager to apply my knowledge to build new user-friendly interfaces, improve app performance, and develop invite and collaboration tools.
+        
+        I appreciate your consideration and invite you to review my portfolio and resume for further details about my experience and skills.
+        
+        Thank you,
+        
+        Tanush Yadav
 
-Best Regards,
-Tanush Yadav
 
     To Start the Message the Company Point of Contact : {contactPoint}
 
